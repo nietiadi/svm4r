3d28c5fd48e4cc86edfe702da9087eb381ea4023
Lan Zhang <zzz@163.com>
2019-11-28 15:29:30 +0800

    test pycharm

--- README.txt
+++ README.txt
@@ -7,5 +7,6 @@
 |--/ttprover/, a truth table prover for teaching
 
 current_job:
+how to write a unit-test
 get familar with codes and probably rewrite some;
-according to the rows of the csv file, create input files for ctl-rp
+according to the rows of the csv file, create input files for ctl-rp;
--- clauses.py
+++ clauses.py
@@ -4,44 +4,48 @@ import sys
 
 from pprint import pprint
 
+
 def generate_all_possible_clauses(num_of_variables=2):
-  """ format:
-  number,p1's value,p2's value
-  1,0,0 => 1, empty clause
-  2,1,-1 => 2, p1 v ~p2
-  """
-  if (num_of_variables>10):
-    print("Error: Too many propositions. "+
-    "The number of propositions should be less than or equal to 10")
-    return
+    """ format:
+    number,p1's value,p2's value
+    1,0,0 => 1, empty clause
+    2,1,-1 => 2, p1 v ~p2
+    """
+    if (num_of_variables > 10):
+        print("Error: Too many propositions. " +
+              "The number of propositions should be less than or equal to 10")
+        return
 
-  clauses = list()
-  for i in range(0, num_of_variables):
-    clauses = add_another_variable(clauses)
-  for i in range(0, len(clauses)):
-    clauses[i].insert(0, i) #insert line number
-  write_clauses(clauses, num_of_variables)
+    clauses = list()
+    for i in range(0, num_of_variables):
+        clauses = add_another_variable(clauses)
+    for i in range(0, len(clauses)):
+        clauses[i].insert(0, i)  # insert line number
+    write_clauses(clauses, num_of_variables)
 
-def add_another_variable(clauses:list) -> list:
-  three = [0, 1, -1]
-  new_list = list()
-  if len(clauses) == 0:
-    new_list = [[0], [1], [-1]]
+
+def add_another_variable(clauses: list) -> list:
+    three = [0, 1, -1]
+    new_list = list()
+    if len(clauses) == 0:
+        new_list = [[0], [1], [-1]]
+        return new_list
+    for c in clauses:
+        for i in range(0, 3):
+            one = list()
+            one.extend(c)
+            one.append(three[i])
+            new_list.append(one)
+            # pprint(c)
     return new_list
-  for c in clauses:
-    for i in range(0, 3):
-      one = list()
-      one.extend(c)
-      one.append(three[i])
-      new_list.append(one)
-      #pprint(c)
-  return new_list
+
 
 def write_clauses(clauses: list, num):
-  import csv
-  with open('data/clauses_for_'+str(num)+'_propositoins.csv', 'wt') as fout:
-    csvout = csv.writer(fout)
-    csvout.writerows(clauses)
+    import csv
+    with open('data/clauses_for_' + str(num) + '_propositoins.csv', 'wt') as fout:
+        csvout = csv.writer(fout)
+        csvout.writerows(clauses)
+
 
 """
 for i in range(-1,2):
@@ -67,22 +71,34 @@ import sys
 generate_all_possible_clauses(int(sys.argv[1]));
 """
 
-def verify_arg(arg):
-  v = True
-  if len(arg)!=2:
-    v = False
-  str = arg[1]
-  if str.isdigits():
-    num = int(str)
-  if not(2<=num<=10):
-    v = False
-  if v:
-    print('The argument is wrong. The argument is an integer between 2 and 10, inclusively')
-    print('Example: python3 clause.py 2')
-    sys.exit(0)
-  else:
-    return num
 
-#main
+def verify_arg(arg):
+    """
+    Verify the argument, which indicates the number of the propositions involved. The number has to be in [2, 10]
+    :param arg: the number of propositions, which a user inputs
+    :return: the valid number of propositions
+    """
+    valid = True
+    num = 0
+    if len(arg) != 2:
+        valid = False
+    else:
+        str = arg[1]
+        if not str.isdigits():
+            valid = False
+        else:
+            num = int(str)
+            if not (2 <= num <= 10):
+                valid = False
+
+    if not valid:
+        print('The argument is wrong. The argument is an integer between 2 and 10, inclusively')
+        print('Example: python3 clause.py 2')
+        sys.exit(0)
+    else:
+        return num
+
+
+# main
 np = verify_arg(sys.argv)
-#generate_all_possible_clauses(2);
+# generate_all_possible_clauses(2);
