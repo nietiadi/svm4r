29a673e6b7d6c8bfbd8bd82083c84ce3be5f2aad
Lan Zhang <zzz@163.com>
2020-08-07 20:21:51 +0800

    test_PLSatProblem_linux.py is problematic

---
+++ test_something.py
@@ -0,0 +1,20 @@
+import unittest
+from pl_sat_problem import PLSatProblem
+
+class TestPLSatProblemLinux(unittest.TestCase):
+    def setUp(self):
+        if PLSatProblem.num_propositions == 0:
+            PLSatProblem.init_class_properties(2)
+
+    def tearDown(self):
+        pass
+
+
+    def test_1_unsat(self):
+        problem = PLSatProblem('0,0,1,0,0,1,0,0')
+        my_cal = 'unsat'
+        problem.run_ctlrp()
+        self.assertEqual(my_cal, problem.sat)
+
+
+unittest.main()
